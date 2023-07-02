4c17243075d8b178074c0be28e228e90de8b5477
Andrey Verbitskiy <andreyverbitskiy@Andreys-MBP.Dlink>
2023-07-02 20:13:41 +0300

    решение первого занятия

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
   // ваш код...
+  switch (str) {
+    case '':
+      return str
+    case str.length === 1:
+      return str.toUpperCase()
+    case str:
+      return str[0].toUpperCase() + str.slice(1)
+  }
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
   // ваш код...
+  let spamWord1 = '1xBet'
+  let spamWord2 = 'XXX'
+  let strToLowerCase = str.toLowerCase()
+
+  if (strToLowerCase.includes(spamWord1.toLowerCase()) || strToLowerCase.includes(spamWord2.toLowerCase())) {
+    return true
+  } else {
+    return false
+  }
 }
